434245f506363b5757a88e91fec3b95fc293455e
Ayoze Hernandez <ayoze.hernandez@thoughtworks.com>
2024-09-05 14:37:15 +0100

    Add direct access to hello plugin in the sidebar

--- packages/app/src/components/Root/Root.tsx
+++ packages/app/src/components/Root/Root.tsx
@@ -29,6 +29,7 @@ import { MyGroupsSidebarItem } from '@backstage/plugin-org';
 import GroupIcon from '@material-ui/icons/People';
 
 import CategoryIcon from '@material-ui/icons/Category';
+import EmojiPeopleIcon from '@material-ui/icons/EmojiPeople';
 
 const useSidebarLogoStyles = makeStyles({
   root: {
@@ -78,6 +79,7 @@ export const Root = ({ children }: PropsWithChildren<{}>) => (
         <SidebarItem icon={ExtensionIcon} to="api-docs" text="APIs" />
         <SidebarItem icon={LibraryBooks} to="docs" text="Docs" />
         <SidebarItem icon={CreateComponentIcon} to="create" text="Create..." />
+        <SidebarItem icon={EmojiPeopleIcon} to="hello" text="Hello" />
         {/* End global nav */}
         <SidebarDivider />
         <SidebarScrollWrapper>
